933bab671fc1e91e38653fd3c0730df50d840a22
Vero-L031 <00117526@uca.edu.sv>
2026-09-08 14:51:37 -0600

    RetirarStock

--- GrupoLaboratorio1-E.cc
+++ GrupoLaboratorio1-E.cc
@@ -1,7 +1,30 @@
 #include <iostream>
 
+
+
+
+//Funciones
+void RetirarStock (int *stock, int cantidad);
+
+
 int main(){
 
 
+
+   int Stockretiro = 0; 
+    std::cout << "\n¿Cuanta cantidad de stock deseas retirar ?: ";
+    std::cin >> Stockretiro; 
+
+   
+        RetirarStock(&pro.cantidad, Stockretiro); 
+  
     return 0;
+}
+
+
+
+// Paso por puntero
+void RetirarStock(int *stock, int cantidad)
+{
+    *stock -= cantidad;
 }
