6eb138a3b7f162386082ba5ffcaf52e469961057
gchavez96 <00069926@uca.edu.sv>
2026-09-08 14:47:31 -0600

    feat(stock): implementar registro y validación de stock inicial de productos
    Agregar estructura Producto para manejar nombre y stock
    
    Crear función con paso por referencia para actualizar el inventario
    
    Añadir validación para limitar el ingreso a un máximo de 50 unidades

--- GrupoLaboratorio1-E.cc
+++ GrupoLaboratorio1-E.cc
@@ -1,7 +1,39 @@
 #include <iostream>
+#include <string>
+
+
+struct Producto{
+    std::string nombreProducto;
+    int stock;
+};
+
+void IngresarStock(int &stock, int cantidad);
 
 int main(){
-
+    int cantidad;
+    Producto pro;
+    std::cout<<"Bienvenido, igrese un producto: ";
+    std::cin>>pro.nombreProducto;
+    std::cout<<"Cantidad del producto que decea ingresar: ";
+    std::cin>>cantidad;
+    if (cantidad>50)
+    {
+        std::cout<<"La cantidad no debe superar 50 unidades del producto";
+    }
+    else
+    {
+        pro.stock = 0;
+        IngresarStock(pro.stock, cantidad);
+        
+        std::cout << "\n      Producto registrado       " << std::endl;
+        std::cout << "Nombre: " << pro.nombreProducto << std::endl;
+        std::cout << "Stock total: " << pro.stock << std::endl;
+    }
+    
 
     return 0;
+}
+
+void IngresarStock(int &stock, int cantidad){
+    stock += cantidad;
 }
