cc1b79c377a77a561625162cffd5005268eae58e
ALandaverde77 <00034026@uca.edu.sv>
2026-09-08 14:55:31 -0600

    fix: arreglando error del stock

--- GrupoLaboratorio1-E.cc
+++ GrupoLaboratorio1-E.cc
@@ -1,7 +1,18 @@
 #include <iostream>
+#include <string>
+
+
+void ConsultarsStock(int stock); 
+void ValidacionOperacion (string mensaje);
+
 
 int main(){
 
 
     return 0;
 }
+
+void ConsultarsStock(int stock){
+    std::cout<<"Stock disponible: "<<stock<<" unidades. "<< std::endl; 
+};
+
